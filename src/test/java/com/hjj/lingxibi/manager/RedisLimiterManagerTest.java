7add796725bb11cf225a8eb0fe1577d80385b819
C1own <1765393959@qq.com>
2024-02-05 22:04:08 +0800

    add excel读取和转换 add 实现智能接口（根据输入数据和AI生成图表和结论） add 文件校验、Redisson限流 add 基于线程池实现图表异步生成 add 基于RabbitMQ实现图表异步生成 add 数据库表SQL语句 add 我的图表缓存预热和未从缓存查到数据的缓存写入 add 添加死信队列

--- src/test/java/com/hjj/lingxibi/manager/RedisLimiterManagerTest.java
+++ src/test/java/com/hjj/lingxibi/manager/RedisLimiterManagerTest.java
@@ -13,12 +13,7 @@ class RedisLimiterManagerTest {
     @Test
     void doRateLimit() throws InterruptedException {
         String userId = "1";
-        for (int i=0;i<2;i++) {
-            redisLimiterManager.doRateLimit(userId);
-            System.out.println("success");
-        }
-        Thread.sleep(2000);
-        for (int i=0;i<5;i++) {
+        for (int i=0;i<10;i++) {
             redisLimiterManager.doRateLimit(userId);
             System.out.println("success");
         }
